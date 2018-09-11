ef492022155538a893137d78dcbd83b1b831f081
FJAR <cursos.fjar@gmail.com>
2018-09-11 17:05:18 +0200

    Modificaciones en los módulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 
 
-import { PagesComponent } from './pages/pages.component';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { RegisterComponent } from './login/register.component';
 
-const APP_ROUTES: Routes = [];
-
-const routes: Routes = [
-    { path: '',
-      component: PagesComponent,
-      children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progress', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      ]
-    },
+const  APP_ROUTES: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
 ];
 
-export const APPROUTING = RouterModule.forRoot(routes, { useHash: true });
+export const APPROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
